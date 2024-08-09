c54ac170f15861f749d51223baf55374f0d97be8
Darron Manning <darronmanning@yahoo.com>
2024-08-09 13:48:45 -0500

    finished and completed

--- VariablesExercise/Program.cs
+++ VariablesExercise/Program.cs
@@ -1,10 +1,31 @@
 ﻿namespace VariablesExercise
 {
-    public class Program
+
+    internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string dogName; // declaration syntax
+
+            dogName = "Rex"; // Initialized --- assigning a value 
+
+            int dogAge = 5; //Declaring and initializing
+
+            char firstIntial = 'R';
+
+            bool isHungry = true;
+
+            double dogWeight = 54.3;
+
+            decimal dogHeight = 14.7m;
+
+            Console.WriteLine(
+                $"Hello,my dog's name is {dogName}, his initial is {firstIntial} and he is {dogAge} years old.");
+            Console.WriteLine($"He weighs {dogWeight} pounds and his is {dogHeight} inches tall");
+            Console.WriteLine($"It is {isHungry.ToString().ToLower()} he is always hungry.");
         }
     }
 }
+    
+    
+
